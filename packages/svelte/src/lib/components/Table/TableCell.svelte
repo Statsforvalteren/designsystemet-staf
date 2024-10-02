a8ec2cbfc7b96c78a14ba910b89e5f8d35a3830c
TobiasGunther <tobiasg@egde.no>
2024-10-02 09:33:00 +0200

    fix: added onClick event handler to TableCell.

--- packages/svelte/src/lib/components/Table/TableCell.svelte
+++ packages/svelte/src/lib/components/Table/TableCell.svelte
@@ -1,3 +1,11 @@
-<td {...$$restProps}>
+<script>
+  /**
+   * Event handler for click event.
+   * @type {(event: MouseEvent) => void}
+   */
+  export let onClick = () => {};
+</script>
+
+<td on:click on:click={onClick} {...$$restProps}>
   <slot />
 </td>
